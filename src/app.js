bedd42371ca3c0314a96a766bd3139a956787116
shhwetaveer <veershweta2003@gmail.com>
2025-09-18 19:44:24 +0530

    Update and delete

--- src/app.js
+++ src/app.js
@@ -3,25 +3,76 @@ const connectDB = require('./config/database');
 const app = express();
 const User = require('./models/user');
 
-app.post("/signup", async (req,res) => {
-    //Creating a new instance of the user model
-    const user = new User({
-        fisrtName :"Sonali",
-        LastName : "Veer",
-        emailId : "sona@gmail.com",
-        password: "sona@123"
-    });
+app.use(express.json());
 
-    await user.save();
-    res.send("User Added Successfully 2");
+app.post("/signup", async (req, res) => {
+    //Creating a new instance of the user model
+    const user = new User(req.body);
+    try {
+        await user.save();
+        res.send("User Added Successfully ");
+    } catch (err) {
+        res.status(500).send("Error adding user", err.messa);
+    }
+});
+//getUser by email
+app.get("/user", async (req, res)=>{
+    const userEmail = req.body.emailId;
+    try {
+       const user = await User.find({emailId: userEmail});
+       if(user.length === 0){
+        res.status(404).send("User not found");
+       } else{
+        res.send(user);
+       }
+        
+    }
+    catch(err){
+        res.status(400).send("Something went Wrong");
+    }
+});
+
+//Feed API- GET /feed- get allthe usres from the database
+app.get("/feed",async(req,res)=>{
+
+try{
+    const users = await User.find({});
+    res.send(users);
+}
+catch(err){
+    res.status(500).send("Error fetching users");}
+
+});
+
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId;
+    
+try{
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User Deleted Successfully");
+}
+catch(err){
+    res.status(500).send("Error fetching users");}
+
 })
+
+app.patch("/user", async (req, res) =>{
+    const userId = req.body.userId;
+    const data = req.body;
+    try{
+    await User.findByIdAndUpdate({_id:userId}, data);
+    res.send("User Updated Successfully");
+}
+catch(err){
+    res.status(400).send("Something went wrong");}
+});
 
 connectDB()
     .then(() => {
         console.log("Database connected successfully");
         app.listen(4444, () => {
-    console.log('Running the server on port 4444');
-});
+            console.log('Running the server on port 4444');
+        });
     })
     .catch((err) => {
         console.error("Error connecting to the database");
