3550087b3b7ab58541255d065e4f525e04c72473
shhwetaveer <veershweta2003@gmail.com>
2026-01-11 23:48:13 +0530

    starting frontend

--- src/routes/user.js
+++ src/routes/user.js
@@ -55,12 +55,12 @@ userRouter.get("/request/sent", userAuth, async (req, res) =>{
 });
 
 userRouter.get("/feed", userAuth, async (req, res) =>{
-    try{
+    try{ 
         const loggedInUser = req.user;
 
         const page = parseInt(req.query.page) || 1;
         const limit = parseInt(req.query.limit) || 10;
-        limit = limit > 50?50 : limit;
+        limit = limit > 50?50 : limit;  
         const skip = (page -1)* limit;
 
         const connectionRequest = await ConnectionRequest.find({
