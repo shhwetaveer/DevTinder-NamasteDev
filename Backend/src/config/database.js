09aebc8d605d0c98983ccf055a2c75d0f6d1e13d
shhwetaveer <veershweta2003@gmail.com>
2026-01-22 23:00:58 +0530

    Daisy Ui

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // returns a promise 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://veershweta2003_db_user:ql5qBeXTvCYxZlOF@devtycoon.bo5jvms.mongodb.net/devTinder");
+    await mongoose.connect(process.env.MONGO_DB);
 
 }
 module.exports = connectDB;
